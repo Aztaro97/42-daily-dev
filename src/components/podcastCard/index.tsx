58b8c6697ff71b2bda46b904f9d68e90515f47b4
aztaro97 <aztaro97@gmail.com>
2023-06-15 21:45:10 +0400

    chore: :dizzy: updated podcast card

--- src/components/podcastCard/index.tsx
+++ src/components/podcastCard/index.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import styled from "@emotion/styled"
-import { Button, Card, Tooltip } from "react-daisyui"
+import { Avatar, Button, Card, Tooltip } from "react-daisyui"
 import { AiOutlineLike, AiTwotoneLike } from "react-icons/ai"
 import { BiCommentDots } from "react-icons/bi"
-import { GrView } from "react-icons/gr"
+import { FaHeadphones } from "react-icons/fa"
 import tw from "twin.macro"
 
 function PodCastCard() {
@@ -23,25 +23,25 @@ function PodCastCard() {
           alt="Post"
         />
         <CardBody>
-          <CardDate>April 4, 2023</CardDate>
           <CardTitle tag="h2">
-            After internal 'panic', Google is rushing to unveil a new AI-powered
-            search engine to compete with Microsoft{" "}
+            After internal 'panic', Google is rushing
           </CardTitle>
+
+          <Box>
+            <RecordTime>
+              <FaHeadphones size={18} />
+              <span>23:12</span>
+            </RecordTime>
+            <PublishDate>April 4, 2023</PublishDate>
+          </Box>
+
           <CardActions>
-            <CardAvatar
-              src="https://i.pravatar.cc/300"
-              height={60}
-              width={60}
-              alt="user avatar"
-            />
+            <AvatarGroup space={-10}>
+              <AvatarStyled src="https://i.pravatar.cc/300" size={"xs"} />
+              <AvatarStyled src="https://i.pravatar.cc/300" size={"xs"} />
+              <AvatarStyled src="https://i.pravatar.cc/300" size={"xs"} />
+            </AvatarGroup>
             <CardRightAction>
-              <Tooltip color="primary" message="View">
-                <CardActionIcon>
-                  <GrView size={18} tw="!stroke-gray-400" />
-                  <span>12.6k</span>
-                </CardActionIcon>
-              </Tooltip>
               <Tooltip color="primary" message="Comment">
                 <CardActionIcon tw="border-x border-gray-400 border-opacity-40 px-3">
                   <BiCommentDots size={18} />
@@ -68,21 +68,24 @@ function PodCastCard() {
 
 const CardWrapper = tw(
   Card,
-)`border border-gray-700 hover:border-primary transition duration-500 ease-in-out cursor-pointer`
+)`flex flex-col gap-1 border border-gray-700 hover:border-primary transition duration-500 ease-in-out cursor-pointer`
 const CardTitle = tw(Card.Title)`line-clamp-3 text-white text-2xl mb-0`
-const CardBody = tw(Card.Body)`px-6 pt-2`
-const CardDate = tw.p`text-gray-400 text-opacity-80 text-xs`
+const CardBody = tw(Card.Body)`px-6 pt-2 w-full`
+const PublishDate = tw.p`flex-grow-0 text-gray-400 text-opacity-80 text-xs inline`
 const CardActions = tw(
   Card.Actions,
 )`flex justify-between items-center gap-3 mt-4`
-const CardAvatar = tw(Image)`rounded-full w-8 h-8`
 const CardRightAction = tw.div`flex gap-2`
 const CardActionIcon = styled.div`
-  ${tw`text-gray-400 flex items-center gap-1 px-1`}
+  ${tw`flex items-center gap-1 px-1 text-gray-400`}
 
   & span {
-    ${tw`text-gray-400 text-sm`}
+    ${tw`text-sm text-gray-400`}
   }
 `
+const AvatarStyled = tw(Avatar)`w-8 h-8`
+const AvatarGroup = tw(Avatar.Group)``
+const Box = tw.div`flex items-center justify-between w-full gap-2`
+const RecordTime = tw.div`flex gap-2 items-center text-xs`
 
 export default PodCastCard
