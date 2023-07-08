9279410c337580c816ef4ac35ee1bcf018ca7b96
aztaro97 <aztaro97@gmail.com>
2023-07-08 14:18:59 +0400

    chore: :closed_lock_with_key: custom _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { Head, Html, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const originalRenderPage = ctx.renderPage
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App: any) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component: any) => Component,
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
